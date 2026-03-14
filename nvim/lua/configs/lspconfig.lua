76d0e67a9cdc60b5b841134d2e791ab1f7dca4c0
genwilliam <178803510+genwilliam@users.noreply.github.com>
2026-03-14 13:47:50 +0800

    Refactor Neovim Lua configurations

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,15 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
--- LSP servers for Go, Python, Frontend, etc.
-local servers = {
-  "html",
-  "cssls",
-  "gopls",      -- Go
-  "pyright",    -- Python
-  "ts_ls",      -- TypeScript/JavaScript
-  "eslint",     -- JavaScript/TypeScript linting
-  "jsonls",     -- JSON
-}
+local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,49 +13,16 @@ return {
     end,
   },
 
-  -- TreeSitter for better syntax highlighting and code navigation
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "vim", "lua", "vimdoc",
-        "html", "css", "javascript", "typescript", "tsx",
-        "go", "python", "json", "yaml", "markdown",
-      },
-    },
-  },
-
-  -- DAP (Debugger Adapter Protocol) for debugging
-  {
-    "mfussenegger/nvim-dap",
-    ft = { "go", "python", "javascript", "typescript" },
-    config = function()
-      require "configs.dap"
-    end,
-  },
-
-  -- DAP UI
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-  },
-
-  -- Comments plugin for Ctrl+/
-  {
-    "numToStr/Comment.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      toggler = {
-        line = "gcc",
-        block = "gbc",
-      },
-      opleader = {
-        line = "gc",
-        block = "gb",
-      },
-    },
-  },
-
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
